be8e2be01076d6837402d8f67aa37ee126413c68
Haibo0 <haibo201220@gmail.com>
2022-02-02 18:52:59 -0800

    assignment 2

---
+++ assignment-2.cpp
@@ -0,0 +1,14 @@
+# include<iostream>
+using namespace std;
+
+int main()
+{
+cout<<"We have made a long journey of 4276 miles./n"<<endl;
+
+cout<<"Our checking account balance is -20./n"<<endl;
+
+cout<<"About 189000 days ago Columbus stood on this spot./n"<<endl;
+
+return 0;
+
+}
