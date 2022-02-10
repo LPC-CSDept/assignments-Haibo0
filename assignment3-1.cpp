1f08f215eb55b281258e6bd970cd9ebcaaa961e8
Haibo0 <haibo201220@gmail.com>
2022-02-10 11:10:40 -0800

    3-1

--- assignment3-1.cpp
+++ assignment3-1.cpp
@@ -4,6 +4,24 @@ using namespace std;
 int main()
 {
 
+int classA;
+int classB;
+int classC;
 
-    
+int priceA=15;
+int priceB=12;
+int priceC=9;
+
+int total;
+
+cout <<"number of A:";
+cin>>classA;
+cout <<"number of B:";
+ cin>>classB;  
+cout <<"number of C:";
+ cin>>classC;
+
+ total =(classA*priceA)+ (classB*priceB)+(classC*priceC);
+
+ cout<<"total price:"<<total<<endl;
 }
