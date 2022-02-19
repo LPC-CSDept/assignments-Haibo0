1c915579b959e8d571359c80f865d2f8ec98f167
Haibo0 <haibo201220@gmail.com>
2022-02-18 21:27:18 -0800

    assignment 3-3

---
+++ assignment 3-3.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  float C;
+  cout<<"enter Celsius"<<endl;
+  cin>> C;
+
+  float F;
+  F=9/5.0*C+32;
+  cout<<"Fahrenheit is "<<F<<endl;
+  
+  return 0;
+    
+  
+}
+
+    
