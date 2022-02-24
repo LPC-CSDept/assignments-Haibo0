deb2efcc8395e22932b3053bff8255d1ae78e1a0
Haibo0 <haibo201220@gmail.com>
2022-02-23 20:16:44 -0800

    assignment 3-5.cpp

---
+++ assignment 3-5.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+int main()
+{
+  int num1;
+  int num2;
+  int num3;
+  
+ 
+     num1=rand() %100;
+     cout<<"num1="<<num1<<endl;
+     num2 = rand() %100;
+     cout<<"num2="<<num1<<endl;
+     num3 =rand() %100;
+     cout<<"num3="<<num3<<endl;
+  int sum;
+     cout<<"sum="<<num1+num2+num3<<endl;
+  int average;
+     cout<<"average="<<(num1+num2+num3)/3<<endl;
+    return 0;
+}
