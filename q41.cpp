a718c5dd3419cb9b6eb8b34f6bd6825d9bd79b4a
Haibo0 <97717938+Haibo0@users.noreply.github.com>
2022-02-28 16:45:56 -0800

    Update q41.cpp

--- q41.cpp
+++ q41.cpp
@@ -7,19 +7,19 @@ int main()
   cout<<"Which formula do you want to see?\n\n";
   cout<<"1. Area of a circle\n";
   cout<<"2. Area of a rectangle\n";
-  cout<<"3. Area of a cylinder\n";
+  cout<<"3. Area of a Triangle\n";
   cout<<"4. None of them\n";
   cin>>selections;
   switch(selections)
     {
     case 1:
-      cout<<"Pi times radius squared\n";
+      cout<<"area=Pi times radius squared\n";
       break;
     case 2:
-      cout<<"length times width\n";
+      cout<<"area=length times width\n";
       break;
     case 3:
-      cout<<"Pi time radius squared times height\n";
+      cout<<"area = base * height * 0.5\n";
       break;
       case 4:
       cout<<"Quit\n";
