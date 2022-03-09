01df4cd410163644d6bc7b29137ed9816d28d3d7
Haibo0 <97717938+Haibo0@users.noreply.github.com>
2022-03-08 19:29:23 -0800

    Add files via upload
    lab 5-8

---
+++ assignment 5-1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+  int b,e;
+  int i=1;
+  cout<<"enter 2 interger"<<endl;
+cin>>b>>e;
+
+  int c = 1;
+while(b<=c && c<=e){
+  c = pow(2,i);
+  if(c<e){
+  cout<< c <<endl;
+  i++;  
+  }
+}
+
+    return 0;
+  
+}
