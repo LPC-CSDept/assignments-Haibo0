e601347832ac652aa063f87b2bd7c0330f406816
Haibo0 <haibo201220@gmail.com>
2022-04-12 13:41:38 -0700

    Question 5

---
+++  MId term exam 5.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+int getInput(int &n1, int &n2);
+int isSame(int n1, int n2);
+int intDivision(int n1, int n2);
+int fileWrite(int n1, int n2, int result);
+int main()
+{
+    int n1, n2, result;
+    getInput(n1, n2);
+    result = intDivision(n1, n2);
+    fileWrite(n1, n2, result);
+    return 0;
+}
+int getInput(int &n1, int &n2)
+{
+    cout << "Enter two numbers: ";
+    cin >> n1 >> n2;
+    return 0;
+}
+int isSame(int n1, int n2)
+{
+    if (n1 == n2)
+        return 1;
+    else
+        return 0;
+}
+int intDivision(int n1, int n2)
+{
+    int result;
+    result = n1 / n2;
+    return result;
+}
+int fileWrite(int n1, int n2, int result)
+{
+    ofstream outfile;
+    outfile.open("question5.txt");
+    outfile << "The division of " << n1 << " and " << n2 << " is " << result << endl;
+    outfile.close();
+    return 0;
+}
+
