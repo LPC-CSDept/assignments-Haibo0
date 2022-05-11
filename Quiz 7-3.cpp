5bc2aae9b4d49bb086d684aebdc5f91f51a1f181
Haibo0 <haibo201220@gmail.com>
2022-05-10 21:33:19 -0700

    Quiz 7

--- Quiz 7-3.cpp
+++ Quiz 7-3.cpp
@@ -3,15 +3,15 @@ using namespace std;
 int findmax(int numbers[], int, int); 
 int main()
 { 
-  const int n =10;
+  const int n =10;//declare array of size num 
   int numbers1[n], numbers2[n], max1, max2, a = 0;
   int i;
-  int numbers[n]={10,15,0,-25,19,20,25,30,45,20};
+  int numbers[n]={10,15,0,-25,19,20,25,30,45,20};// element in array
     for(i = 0; i < n; i++) 
     {
       if(i < n/2) 
     {
-      numbers1[i] = numbers[i];      
+      numbers1[i] = numbers[i];  
     }
     else 
     {
@@ -19,10 +19,10 @@ int main()
     }
   }
    
-    max1 = findmax(numbers1, a, n/2); 
-    cout<<"Max number from slot 1 : "<<max1<<"\t"; 
-    max2 = findmax(numbers2, n/2, n); 
-    cout<<"Max number from slot 2 : "<<max2; 
+    max1 = findmax(numbers1, a, n/2); //maximum in first half
+    cout<<"Max number from slot 1 : "<<max1<<"\t"; // printing maximum inn first half on screen
+    max2 = findmax(numbers2, n/2, n); //maximum in second half
+    cout<<"Max number from slot 2 : "<<max2; // printing maximum inn second half on screen
     
   return 0;
   }
@@ -31,9 +31,9 @@ int findmax(int numbers[], int from, int to)
   int i, t = numbers[0];
   for (i = from; i < to; ++i)
 {
-  if (t < numbers[i]) 
+  if (t < numbers[i]) //compare an array for max value if max  < array element than it will assign to max variable    
   {
-    t = numbers[i];
+    t = numbers[i];//assigning greater value to max
   }
 }
 return t; 
