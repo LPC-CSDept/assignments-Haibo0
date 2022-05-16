464400e9ccd3faf729a2d99452ab74763dcb2ca1
Haibo0 <haibo201220@gmail.com>
2022-05-16 16:54:02 -0700

    Quiz 7

--- Quiz 7-1.cpp
+++ Quiz 7-1.cpp
@@ -1,29 +1,24 @@
-#include<cmath>
-#include<iostream>
+#include <cmath>
+#include <iostream>
 using namespace std;
 
 const int N = 10;
-bool check(int a[],int index)
-{
-    
-    if(index==0 && (abs(a[index]-a[index+1])<10))
-         return true;
-   
-    if(index==N-1 && (abs(a[index]-a[index-1])<10))
-        return true;
-  
-    if(index!=N && index!=0 && (abs(a[index]-a[index+1])<10) && (abs(a[index]-a[index-1])<10))
-        return true;
-  
-        return false;
-}
-int main()
-{
-    int number[N] = {10,15,0,-25,19,20,25,30,45,20};
+
+int main(){
+  int number[N] = {10,15,0,-25,19,20,25,30,45,20};
+
+  //for loop to go through each element of the array
+    //if the difference between the elements is less than 10, before the element and behind it. 
+      //then print the element
        
-        for(int i=0;i<N;i++){
-       
-    if(check(number,i))
-        cout<<number[i]<<" at index "<<i<<" is following the criteria."<<endl;
-}
+  for (int i=0; i<N-1; i++){
+    if (i == 0 && (abs(number[i]-number[i+1])<10)){
+      cout << number[i] << endl;
+    }
+    if (i != 0 && (abs(number[i]-number[i+1])<10) && (abs(number[i]-number[i-1])<10)){
+      cout << number[i] << endl;
+    }
+  }
+
+  
 }
